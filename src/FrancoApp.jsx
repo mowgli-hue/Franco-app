2b7c288fae476bef415f5f6515c4b0955bba8c77
Jungle Labs <mowgli@junglelabsworld.com>
2026-04-01 12:30:56 -0700

    Fix extra brace

--- src/FrancoApp.jsx
+++ src/FrancoApp.jsx
@@ -3949,7 +3949,6 @@ function TopBar({screen,onNavigate,companion,progress,user,guestMode,onAuthNav})
     )}
   </div>;
 }
-}
 
 export default function App(){
   return <AuthProvider><AppInner/></AuthProvider>;
