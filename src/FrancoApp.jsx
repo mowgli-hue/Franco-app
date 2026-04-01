7b01d5a62e21978a329f32d02240bc026ffd11c1
Jungle Labs <mowgli@junglelabsworld.com>
2026-03-31 17:48:24 -0700

    Fix extra brace line 3932

--- src/FrancoApp.jsx
+++ src/FrancoApp.jsx
@@ -3929,7 +3929,6 @@ function TopBar({screen,onNavigate,companion,progress,user,guestMode,onAuthNav})
     )}
   </div>;
 }
-}
 
 export default function App(){
   return <AuthProvider><AppInner/></AuthProvider>;
